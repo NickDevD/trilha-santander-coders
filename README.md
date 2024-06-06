3734e91b1633b21c5a0dda705c623caf32e044f5
NickDevD <nick.developerdev@gmail.com>
2024-06-06 12:07:54 -0300

    update 1.2

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Repositório para armazenar conteúdo do curso Santander Coders
 
 # Gravando mudanças no repositório
+
+## Utilizando os comando git status e git diff
--- santander-coders/src/versionamento-codigo/santanderVersionamento.java
+++ santander-coders/src/versionamento-codigo/santanderVersionamento.java
@@ -1,6 +1,10 @@
 public class santanderVersionamento {
 
     public static void main(String[] args) {
+
+        int num1 = 2;
+
+        System.out.println(num1);
         
     }
     
